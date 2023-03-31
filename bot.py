a662d54efbdcab838e664669c1e31dd94596cea4
Brotin Halder <128289788+Brotin27@users.noreply.github.com>
2023-03-31 14:14:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,34 +20,30 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# Telegram Link : https://telegram.dog/Mo_Tech_Group
-# Repo Link : https://github.com/PR0FESS0R-99/Auto-Approved-Bot
-# License Link : https://github.com/PR0FESS0R-99/Auto-Approved-Bot/blob/Auto-Approved-Bot/LICENSE
-
 from os import environ
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, User, ChatJoinRequest
 
-pr0fess0r_99=Client(
+brotinhalder=Client(
     "Auto Approved Bot",
     bot_token = environ["BOT_TOKEN"],
     api_id = int(environ["API_ID"]),
     api_hash = environ["API_HASH"]
 )
 
-CHAT_ID = [int(pr0fess0r_99) for pr0fess0r_99 in environ.get("CHAT_ID", None).split()]
+CHAT_ID = [int(brotinhalder) for brotinhalder in environ.get("CHAT_ID", None).split()]
 TEXT = environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour Auto Approved")
 APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 
-@pr0fess0r_99.on_message(filters.private & filters.command(["start"]))
+@brotinhalder.on_message(filters.private & filters.command(["start"]))
 async def start(client: pr0fess0r_99, message: Message):
     approvedbot = await client.get_me() 
-    button = [[ InlineKeyboardButton("📦 Repo", url="https://github.com/PR0FESS0R-99/Auto-Approved-Bot"), InlineKeyboardButton("Updates 📢", url="t.me/Mo_Tech_YT") ],
+    button = [[ InlineKeyboardButton("Supprot", url="t.me/brotinhalder"), InlineKeyboardButton("Updates 📢", url="t.me/B3botofficial") ],
               [ InlineKeyboardButton("➕️ Add Me To Your Chat ➕️", url=f"http://t.me/{approvedbot.username}?startgroup=botstart") ]]
     await client.send_message(chat_id=message.chat.id, text=f"**__Hello {message.from_user.mention} Iam Auto Approver Join Request Bot Just [Add Me To Your Group Channnl](http://t.me/{approvedbot.username}?startgroup=botstart) || Repo https://github.com/PR0FESS0R-99/Auto-Approved-Bot||**__", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
 
-@pr0fess0r_99.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
-async def autoapprove(client: pr0fess0r_99, message: ChatJoinRequest):
+@brotinhalder.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
+async def autoapprove(client: brotinhalder, message: ChatJoinRequest):
     chat=message.chat # Chat
     user=message.from_user # User
     print(f"{user.first_name} Joined 🤝") # Logs
@@ -57,4 +53,4 @@ async def autoapprove(client: pr0fess0r_99, message: ChatJoinRequest):
     #   print("Welcome....")
 
 print("Auto Approved Bot")
-pr0fess0r_99.run()
+brotinhalder.run()
